f3da9e6cec68797a3e0c77379c42db93479fedea
Nagarjuna Reddy Pothi Reddy <preddy@nisum.com>
2020-05-12 14:35:41 +0530

    Initial Commit for All Microservices

---
+++ orderdetails-service/src/main/java/com/gitlab/project/orderdetailsservice/OrderdetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.project.orderdetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderdetailsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderdetailsServiceApplication.class, args);
+	}
+
+}
---
+++ orderdetails-service/src/test/java/com/gitlab/project/orderdetailsservice/OrderdetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.project.orderdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderdetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
